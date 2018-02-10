9d287ec660959f893cccecb04fe9a858efe5d935
Halkeand <Halkeand@gmail.com>
2018-02-10 12:50:51 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import BarChart from './components/BarChart'
+
+export default class MyComponent extends Component {
+	state = {}
+
+	render() {
+		return (
+			<div>
+				<BarChart />
+			</div>
+		)
+	}
+}
